bb78cd6f6eded3da6a45d5714afb249bbdeb0d5f
AnastasiaWei <61657226+kuznechek@users.noreply.github.com>
2021-07-17 16:39:20 +0300

    File added

---
+++ filescript.cs
@@ -0,0 +1,18 @@
+﻿  
+using System;
+using System.IO;
+namespace my.cool.name
+{
+    internal class MyCoolClass
+    {
+          double one;
+          // { get };
+
+          double two;
+          // { get };
+
+          double three;
+          // { get };
+
+    }
+}
